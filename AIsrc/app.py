ecd52c7cb8e9f8ac5b03408a0e35f52bb74d2e12
ElSergioOliveros <s.oliverosb@uniandes.edu.co>
2023-11-18 19:13:23 -0500

    fileOpeningFunctions

--- AIsrc/app.py
+++ AIsrc/app.py
@@ -3,30 +3,48 @@ import nibabel as nib
 import cv2
 from mayavi import mlab
 from sklearn.preprocessing  import MinMaxScaler
-import vtk
 
-def getMRIScan(imageNum):
+def getTrainData(imageNum):
     scaler = MinMaxScaler()
     
-    path = f"BraTS2020_TrainingData\MICCAI_BraTS2020_TrainingData\BraTS20_Training_{imageNum}"
+    path = f"AIsrc\BraTS2020_ValidationData\MICCAI_BraTS2020_ValidationData\BraTS20_Validation_{imageNum}"
 
-    flair = nib.load(path + f"\BraTS20_Training_{imageNum}_flair.nii").get_fdata()
+    flair = nib.load(path + f"\BraTS20_Validation_{imageNum}_flair.nii").get_fdata()
     flair = scaler.fit_transform(flair.reshape(-1, flair.shape[-1])).reshape(flair.shape)
 
-    t1ce = nib.load(path + f"\BraTS20_Training_{imageNum}_t1ce.nii").get_fdata()
+    t1ce = nib.load(path + f"\BraTS20_Validation_{imageNum}_t1ce.nii").get_fdata()
     t1ce = scaler.fit_transform(t1ce.reshape(-1, t1ce.shape[-1])).reshape(t1ce.shape)
 
-    t1 = nib.load(path + f"\BraTS20_Training_{imageNum}_t1.nii").get_fdata()
+    t1 = nib.load(path + f"\BraTS20_Validation_{imageNum}_t1.nii").get_fdata()
     t1 = scaler.fit_transform(t1.reshape(-1, t1.shape[-1])).reshape(t1.shape)
 
-    t2 = nib.load(path +f"\BraTS20_Training_{imageNum}_t2.nii").get_fdata()
+    t2 = nib.load(path +f"\BraTS20_Validation_{imageNum}_t2.nii").get_fdata()
     t2 = scaler.fit_transform(t2.reshape(-1, t2.shape[-1])).reshape(t2.shape)
 
-    test_mask=nib.load(path + f"\BraTS20_Training_{imageNum}_seg.nii").get_fdata()
+    test_mask=nib.load(path + f"\BraTS20_Validation_{imageNum}_seg.nii").get_fdata()
     test_mask=test_mask.astype(np.uint8)
     test_mask[test_mask>=1] = 255
 
     return flair, t1, t1ce, t2, test_mask
+
+def getValidationData(imageNum):
+    scaler = MinMaxScaler()
+    
+    path = f"AIsrc\BraTS2020_ValidationData\MICCAI_BraTS2020_ValidationData\BraTS20_Validation_{imageNum}"
+
+    flair = nib.load(path + f"\BraTS20_Validation_{imageNum}_flair.nii").get_fdata()
+    flair = scaler.fit_transform(flair.reshape(-1, flair.shape[-1])).reshape(flair.shape)
+
+    t1ce = nib.load(path + f"\BraTS20_Validation_{imageNum}_t1ce.nii").get_fdata()
+    t1ce = scaler.fit_transform(t1ce.reshape(-1, t1ce.shape[-1])).reshape(t1ce.shape)
+
+    t1 = nib.load(path + f"\BraTS20_Validation_{imageNum}_t1.nii").get_fdata()
+    t1 = scaler.fit_transform(t1.reshape(-1, t1.shape[-1])).reshape(t1.shape)
+
+    t2 = nib.load(path +f"\BraTS20_Validation_{imageNum}_t2.nii").get_fdata()
+    t2 = scaler.fit_transform(t2.reshape(-1, t2.shape[-1])).reshape(t2.shape)
+
+    return flair, t1, t1ce, t2
 
 
 def getBrainContours(image):
