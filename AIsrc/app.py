95156124f1cbb95c38dc3a3295bbf0f69d89176f
ElSergioOliveros <s.oliverosb@uniandes.edu.co>
2023-11-19 20:37:27 -0500

    3DModelCreator

--- AIsrc/app.py
+++ AIsrc/app.py
@@ -1,8 +1,9 @@
 import numpy as np
 import nibabel as nib
 import cv2
-from mayavi import mlab
 from sklearn.preprocessing  import MinMaxScaler
+from skimage import measure
+import trimesh
 
 def getTrainData(imageNum):
     scaler = MinMaxScaler()
@@ -10,22 +11,25 @@ def getTrainData(imageNum):
     path = f"AIsrc\BraTS2020_TrainingData\MICCAI_BraTS2020_TrainingData\BraTS20_Training_{imageNum}"
 
     flair = nib.load(path + f"\BraTS20_Training_{imageNum}_flair.nii").get_fdata()
-    flair = scaler.fit_transform(flair.reshape(-1, flair.shape[-1])).reshape(flair.shape)
-
-    t1ce = nib.load(path + f"\BraTS20_Training_{imageNum}_t1ce.nii").get_fdata()
-    t1ce = scaler.fit_transform(t1ce.reshape(-1, t1ce.shape[-1])).reshape(t1ce.shape)
+    flair = scaler.fit_transform(flair.reshape(-1, flair.shape[-1])).reshape(flair.shape)*255
+    flair = flair.astype("uint8")
 
     t1 = nib.load(path + f"\BraTS20_Training_{imageNum}_t1.nii").get_fdata()
-    t1 = scaler.fit_transform(t1.reshape(-1, t1.shape[-1])).reshape(t1.shape)
+    t1 = scaler.fit_transform(t1.reshape(-1, t1.shape[-1])).reshape(t1.shape)*255
+    t1 = t1.astype("uint8")     
+
+    t1ce = nib.load(path + f"\BraTS20_Training_{imageNum}_t1ce.nii").get_fdata()
+    t1ce = scaler.fit_transform(t1ce.reshape(-1, t1ce.shape[-1])).reshape(t1ce.shape)*255
+    t1ce = t1ce.astype("uint8")   
 
     t2 = nib.load(path +f"\BraTS20_Training_{imageNum}_t2.nii").get_fdata()
-    t2 = scaler.fit_transform(t2.reshape(-1, t2.shape[-1])).reshape(t2.shape)
+    t2 = scaler.fit_transform(t2.reshape(-1, t2.shape[-1])).reshape(t2.shape)*255
+    t2 = t2.astype("uint8")
 
-    test_mask=nib.load(path + f"\BraTS20_Training_{imageNum}_seg.nii").get_fdata()
-    test_mask=test_mask.astype(np.uint8)
-    test_mask[test_mask>=1] = 255
+    mask = nib.load(path + f"\BraTS20_Training_{imageNum}_seg.nii").get_fdata()
+    mask = mask.astype(np.uint8)
 
-    return flair, t1, t1ce, t2, test_mask
+    return flair, t1, t1ce, t2, mask
 
 def getValidationData(imageNum):
     scaler = MinMaxScaler()
@@ -33,16 +37,20 @@ def getValidationData(imageNum):
     path = f"AIsrc\BraTS2020_ValidationData\MICCAI_BraTS2020_ValidationData\BraTS20_Validation_{imageNum}"
 
     flair = nib.load(path + f"\BraTS20_Validation_{imageNum}_flair.nii").get_fdata()
-    flair = scaler.fit_transform(flair.reshape(-1, flair.shape[-1])).reshape(flair.shape)
-
-    t1ce = nib.load(path + f"\BraTS20_Validation_{imageNum}_t1ce.nii").get_fdata()
-    t1ce = scaler.fit_transform(t1ce.reshape(-1, t1ce.shape[-1])).reshape(t1ce.shape)
+    flair = scaler.fit_transform(flair.reshape(-1, flair.shape[-1])).reshape(flair.shape)*255
+    flair = flair.astype("uint8")
 
     t1 = nib.load(path + f"\BraTS20_Validation_{imageNum}_t1.nii").get_fdata()
-    t1 = scaler.fit_transform(t1.reshape(-1, t1.shape[-1])).reshape(t1.shape)
+    t1 = scaler.fit_transform(t1.reshape(-1, t1.shape[-1])).reshape(t1.shape)*255
+    t1 = t1.astype("uint8")
+
+    t1ce = nib.load(path + f"\BraTS20_Validation_{imageNum}_t1ce.nii").get_fdata()
+    t1ce = scaler.fit_transform(t1ce.reshape(-1, t1ce.shape[-1])).reshape(t1ce.shape)*255
+    t1ce = t1ce.astype("uint8")
 
     t2 = nib.load(path +f"\BraTS20_Validation_{imageNum}_t2.nii").get_fdata()
-    t2 = scaler.fit_transform(t2.reshape(-1, t2.shape[-1])).reshape(t2.shape)
+    t2 = scaler.fit_transform(t2.reshape(-1, t2.shape[-1])).reshape(t2.shape)*255
+    t2 = t2.astype("uint8")
 
     return flair, t1, t1ce, t2
 
@@ -52,54 +60,60 @@ def getBrainContours(image):
     onlyContours = np.zeros((rows, cols, floors), dtype="uint8")
 
     for i in range(floors):
-        ithFloor = image[:, :, i].copy()
-        ithFloor = ithFloor*255
-        ithFloor = np.around(ithFloor)
-        ithFloor = ithFloor.astype('uint8')
 
-        cannyImg = cv2.Canny(ithFloor, 100, 120)
+        cannyImg = cv2.Canny(image[:, :, i], 100, 120)
         cannyImg = cv2.dilate(cannyImg, None)
         contours, hierarchy = cv2.findContours(cannyImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         
-
         contourImg = np.zeros((rows, cols))
 
         cv2.drawContours(contourImg, contours, -1, (255), 1)
 
         onlyContours[:, :, i] = contourImg
 
-        cv2.imshow("o", image[:, :, i])
-        cv2.imshow("!", onlyContours[:, :, i])
-        cv2.waitKey(100)
-
     return onlyContours
 
+def getBooleanMasks(mask):
+    onesMask = mask == 1
+    onesMask[:, :, -1] = False  
+    onesMask[:, :, 0] = False
 
-flair, t1, t1ce, t2, test_mask = getTrainData("001")
-brainEdge  = getBrainContours(t1)
+    twosMask = mask == 2
+    twosMask[:, :, -1] = False
+    twosMask[:, :, 0] = False
+    
+    foursMask = mask == 4
+    foursMask[:, :, -1] = False
+    foursMask[:, :, 0] = False
 
-data = brainEdge + test_mask
+    return onesMask, twosMask, foursMask
 
-# imageData = vtk.vtkImageData()
-# imageData.SetDimensions(data.shape[0], data.shape[1], data.shape[2])
-# imageData.SetOrigin(0, 0, 0)
+def create3DMesh(mask, vertColors = np.array([0,0,0]), alpha = 255):
+    vertices, faces, normals, values = measure.marching_cubes(mask)
+    
+    rows, cols = faces.shape
+    colors = np.zeros((rows, 4))
+    colors[:, :3] = vertColors 
+    colors[:, 3] = alpha
+    mesh = trimesh.Trimesh(vertices = vertices, faces=faces, face_colors=colors)
+    return mesh
 
-# dataArray = vtk.vtkFloatArray()
-# dataArray.SetVoidArray(data, 1)
-# imageData.SetPointData(dataArray.GetPointer())
+def create3DBrainWithTumor_Train(file):
+    flair, t1, t1ce, t2, mask = getTrainData(file)
+    
+    brainContour = getBrainContours(t1)
 
+    one, two, four = getBooleanMasks(mask)
 
-# writer = vtk.vtkFileWriter()
-# writer.SetFileName("scalarData.vtk")
-# writer.Write()
+    brainContourMesh = create3DMesh(brainContour==255, alpha=70)
+    onesMesh = create3DMesh(one, vertColors=np.array([255,0,0]), alpha=150)
+    twosMesh = create3DMesh(two, vertColors=np.array([0,255,0]), alpha=100)
+    foursMesh = create3DMesh(four, vertColors=np.array([0,0,255]), alpha=115)
+    meshes = [onesMesh, foursMesh, twosMesh, brainContourMesh]
+    combined = trimesh.util.concatenate(meshes)
+    combined.show()
+    combined.export("test.obj")
+    combined.export("test.glb")
+    combined.export("test.stl")
 
-fig = mlab.figure(size=(800, 800))
-src = mlab.pipeline.scalar_field(data)
-vol = mlab.pipeline.volume(src)
-
-
-
-
-mlab.colorbar()
-mlab.outline()
-mlab.show()
+create3DBrainWithTumor_Train("186")
